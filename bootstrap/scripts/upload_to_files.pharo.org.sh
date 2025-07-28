950b997d596f930269f1d9a091d5928819293e35
Pablo Tesone <tesonep@gmail.com>
2025-07-28 17:22:46 +0200

    Fixing upload script

--- bootstrap/scripts/upload_to_files.pharo.org.sh
+++ bootstrap/scripts/upload_to_files.pharo.org.sh
@@ -3,7 +3,13 @@
 set -ex
 
 SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
-. ${SCRIPTS}/envvars.sh
+. ${SCRIPTS}/envversion.sh
+
+# I will use the name of the image to determine the vm version (because file name is in the format Pharo7.0.0-rc1)
+#
+PHARO_NAME_PREFIX=$(find . -name "Pharo*-bootstrap*.zip" | head -n 1 | cut -d'/' -f 2 | cut -d'-' -f 1-2)
+PHARO_MAJOR="$(git describe --tags --first-parent | cut -d'-' -f 1 | cut -c 2- | cut -d'.' -f 1-1)"
+PHARO_SHORT_VERSION="${PHARO_MAJOR}0"
 
 PACKAGED_VERSIONS="latest*.zip"
 if [ $(is_release_build) == 1 ]; then
